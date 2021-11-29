81c4cde079dec1cc9579b28f8fb885dac4c9386d
Alexander Sidorov <pl4zie@gmail.com>
2021-11-30 00:42:02 +0300

    Add yaml parser

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-
+import json
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
@@ -8,18 +8,29 @@ def main():
     parser.add_argument('second_file', type=str)
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(generate_diff(args.first_file, args.second_file)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
     main()
 
-def generate_diff(file1, file2):
-    json1 = json.load(open('~/python-project-lvl2/gendiff/tests/file1.json'))
-    json2 = json.load(open('~/python-project-lvl2/gendiff/tests/file2.json'))
+def generate_diff(file_path1, file_path2):
+    
+    # identify format of input files (for parsing)
+    if file_path1.endswith('.json') and file_path2.endswith('.json'):
+        encoded_file1 = json.load(open('file1')
+        encoded_file2 = json.load(open('file2')
+
+
+    yaml = ('.yaml', '.yml')   
+    if file_path1.endswith(yaml) and file_path2.endswith(yaml):
+
+    
+
     common_keys = json1.keys() & json2.keys()
     first_only = json1.keys() - json2.keys()
     second_only = json2.keys() - json1.keys()
+    
     result = ["{",]
     for key in common_keys:
         if json1[key] == json2[key]:
@@ -32,8 +43,6 @@ def generate_diff(file1, file2):
     for key in second_only:
         result.append(f"+ {key}: {json2[key]}")
     result.append("}")
-    for item in result:
-        item += "\n"
-    return result
 
-
+    result_str = "\n".join(x for x in result)
+    return result_str
