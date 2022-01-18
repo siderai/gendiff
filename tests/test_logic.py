1c6d564edae0e4b51cabeaf9cf11cad6b2e28efe
Alexander Sidorov <pl4zie@gmail.com>
2022-01-18 17:09:16 +0300

    Update tests

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -7,34 +7,34 @@ import yaml
 
 @pytest.fixture
 def json1():
-    return '/home/siderai/gendiff/tests/fixtures/file1.json'
+    return 'tests/fixtures/file1.json'
 
 @pytest.fixture
 def json2():
-    return '/home/siderai/gendiff/tests/fixtures/file2.json'
+    return 'tests/fixtures/file2.json'
 
 @pytest.fixture
 def jsoncomplex1():
-    return '/home/siderai/gendiff/tests/fixtures/complex1.json'
+    return 'tests/fixtures/complex1.json'
 
 @pytest.fixture
 def jsoncomplex2():
-    return '/home/siderai/gendiff/tests/fixtures/complex2.json'
+    return 'tests/fixtures/complex2.json'
 
 @pytest.fixture
 def yaml1():
-    return '/home/siderai/gendiff/tests/fixtures/file1.yaml'
+    return 'tests/fixtures/file1.yaml'
 
 @pytest.fixture
 def yaml2():
-    return '/home/siderai/gendiff/tests/fixtures/file2.yaml'
+    return 'tests/fixtures/file2.yaml'
 
 @pytest.fixture
 def yamlcomplex1():
-    return '/home/siderai/gendiff/tests/fixtures/complex1.yaml'
+    return 'tests/fixtures/complex1.yaml'
 @pytest.fixture
 def yamlcomplex2():
-    return '/home/siderai/gendiff/tests/fixtures/complex2.yaml'
+    return 'tests/fixtures/complex2.yaml'
 
 paths = (json1, json2, jsoncomplex1, jsoncomplex2, yaml1, yaml2, yamlcomplex1, yamlcomplex2)
 
@@ -49,6 +49,7 @@ def test_decoded(json1, json2, jsoncomplex1, jsoncomplex2, yaml1, yaml2, yamlcom
     assert decoded(yaml2) == yaml.load(open(yaml2), Loader=yaml.Loader)
     assert decoded(yamlcomplex1) == yaml.load(open(yamlcomplex1), Loader=yaml.Loader)
     assert decoded(yamlcomplex2) == yaml.load(open(yamlcomplex2), Loader=yaml.Loader)
+
 
 def test_is_dict():
     assert is_dict(dict()) == True
