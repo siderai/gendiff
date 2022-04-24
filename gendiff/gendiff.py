75465bcfe98159f8717d1dec5d114dacd3d8db7a
Alexander Sidorov <sidai@bk.ru>
2022-04-24 17:27:18 +0300

    Refactor

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,26 +3,27 @@ import argparse
 from .logic import decoded, compared, format_stylish, format_plain, format_json
 
 
-def generate_diff(filepath1, filepath2, format_='stylish'):
+def generate_diff(filepath1, filepath2, format_="stylish"):
     file1, file2 = decoded(filepath1), decoded(filepath2)
     diff = compared(file1, file2)
-    if format_ == 'json':
+    if format_ == "json":
         return format_json(diff)
-    elif format_ == 'plain':
+    elif format_ == "plain":
         return format_plain(diff)
     else:
         return format_stylish(diff)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file', type=str)
-    parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', '--format', default='stylish',
-                        help='set format of output')
+    parser = argparse.ArgumentParser(description="Generate diff")
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    parser.add_argument(
+        "-f", "--format", default="stylish", help="set format of output"
+    )
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file, format_=args.format))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- gendiff/logic.py
+++ gendiff/logic.py
@@ -1,24 +1,18 @@
 import json
 import copy
-from typing import List
+from typing import List, Any
 
 import yaml
 
 
 def decoded(filepath: str) -> dict:
-    """ Yaml/json parser. """
-    if filepath.endswith('.json'):
+    """Yaml/json parser."""
+    if filepath.endswith(".json"):
         return json.load(open(filepath))
-    elif filepath.endswith(('.yaml', '.yml')):
-        return yaml.load(open(filepath, mode='r'), Loader=yaml.Loader)
+    elif filepath.endswith((".yaml", ".yml")):
+        return yaml.load(open(filepath, mode="r"), Loader=yaml.Loader)
     else:
-        raise Exception('Json/yaml parsing error!')
-
-
-def is_dict(obj) -> bool:
-    if isinstance(obj, dict):
-        return True
-    return False
+        raise Exception("Json/yaml parsing error!")
 
 
 def compared(file1: dict, file2: dict) -> dict:
@@ -38,7 +32,7 @@ def compared(file1: dict, file2: dict) -> dict:
         second_only = file2.keys() - file1.keys()
     # create image of difference
     else:
-        raise Exception('Json/yaml parsing error!')
+        raise Exception("Json/yaml parsing error!")
     diff = {}
     # compare common keys
     for key in common_keys:
@@ -46,34 +40,108 @@ def compared(file1: dict, file2: dict) -> dict:
         value_in_second = file2[key]
         # mark equal items
         if value_in_first == value_in_second:
-            diff[('=', key)] = value_in_first
+            diff[("=", key)] = value_in_first
         # mark keys that need recursive comparison of values
         elif is_dict(value_in_first) and is_dict(value_in_second):
-            diff[(' ', key)] = compared(value_in_first, value_in_second)
+            diff[(" ", key)] = compared(value_in_first, value_in_second)
         else:
             # mark initial and updated value of a common key
-            diff[('-', key)] = value_in_first
-            diff[('+', key)] = value_in_second
+            diff[("-", key)] = value_in_first
+            diff[("+", key)] = value_in_second
     # mark deleted keys
     for key in first_only:
-        diff[('-', key)] = file1[key]
+        diff[("-", key)] = file1[key]
     # mark added keys
     for key in second_only:
-        diff[('+', key)] = file2[key]
+        diff[("+", key)] = file2[key]
     return diff
 
 
-def stylish_sorted_str(blank: list, depth: int, indenter: str) -> str:
-    """Data collector that finalizes stylish formatter"""
-    sorter = 4 * depth
-    blank = sorted(blank, key=lambda x: x[sorter:])
-    blank.insert(0, '{')
-    blank.append(indenter + '}')
-    result = '\n'.join(blank)
+def format_json(diff: dict) -> str:
+    """Return diff as json"""
+    blank = dict()
+    for sign, key in diff:
+        keyword = f"{sign} {key}"
+        equal_keyword = f"  {key}"
+        value = diff[(sign, key)]
+        if sign == "=":
+            blank[equal_keyword] = value
+        elif sign == " ":
+            blank[keyword] = format_stylish(value)
+        else:
+            blank[keyword] = value
+    result = json.dumps(blank)
     return result
 
 
-def get_value_from(value_view):
+def format_plain(diff: dict) -> str:
+    """Human-friendly output formatter"""
+    blank: List[str] = list()
+    ancestry: List[str] = list()
+    walk_plain(diff, ancestry, blank)
+    result = "\n".join(sorted(blank, key=lambda x: x[10:]))
+    return result
+
+
+def format_stylish(diff: dict, depth=1) -> str:
+    """Convert diff to json-like string."""
+    blank = list()
+    indenter = " " * 4 * (depth - 1)
+    # iterate diff complex keys
+    for sign, key in diff:
+        value_view = diff[(sign, key)]
+        if sign == " ":
+            next_lvl = depth + 1
+            # sign ' ' means we need to run recursion
+            # to get value
+            value = format_stylish(value_view, next_lvl)
+            line = f"    {key}: {value}"
+        else:
+            value = _parse_value(value_view, depth)
+        # generate whole line
+        if sign == "=":
+            # key didn't change
+            line = f"    {key}: {value}"
+            blank.append(indenter + line)
+        elif sign == "-":
+            line = f"  {sign} {key}: {value}"
+            try:
+                # key updated
+                comparison_value = diff[("+", key)]
+                comparison_value = _parse_value(comparison_value, depth)
+                line2 = f"  + {key}: {comparison_value}"
+                blank.append(indenter + line + "\n" + indenter + line2)
+            except KeyError:
+                # key removed
+                blank.append(indenter + line)
+        else:
+            try:
+                comparison_value = diff[("-", key)]
+                continue
+            except KeyError:
+                # key added
+                line = f"  {sign} {key}: {value}"
+                blank.append(indenter + line)
+
+    return _stylish_sorted_str(blank, depth, indenter)
+
+
+def _stylish_sorted_str(blank: List[str], depth: int, indenter: str) -> str:
+    """Data collector that finalizes stylish formatter.
+    Contecatenates diff pieces from blank, then adds indents for readability
+    """
+    sorter = 4 * depth
+    blank = sorted(blank, key=lambda x: x[sorter:])
+    blank.insert(0, "{")
+    blank.append(indenter + "}")
+    result = "\n".join(blank)
+    return result
+
+
+def _get_value_from(value_view):
+    """Type dependent parser for flat values.
+    As json and yaml use different keywords for bool (false, true)
+    and None (null), we should put them into final diff view"""
     if isinstance(value_view, bool) or value_view is None:
         value = json.dumps(value_view)
     else:
@@ -81,163 +149,101 @@ def get_value_from(value_view):
     return value
 
 
-def stylish_formatted_equals(node, depth=1) -> str:
-    """Convert node that has no diff info into
-    formatted string (as part of stylish formatter)"""
+def _stylish_formatted_equals(node, depth=1) -> str:
+    """Parser for nodes that have no diff sign from after compared().
+    Keys are equal, but keys need recursive comparison.
+    Produces formatted string (to be used part of stylish formatter)"""
     children = list()
-    indenter = ' ' * 4 * (depth - 1)
+    indenter = " " * 4 * (depth - 1)
     for key in node:
         item = node[key]
         if is_dict(item):
             next_lvl = depth + 1
-            value = stylish_formatted_equals(item, next_lvl)
-            line = f'    {key}: {value}'
+            value = _stylish_formatted_equals(item, next_lvl)
+            line = f"    {key}: {value}"
         else:
-            value = get_value_from(item)
-            line = f'    {key}: {value}'
+            value = _get_value_from(item)
+            line = f"    {key}: {value}"
         children.append(indenter + line)
-    result = stylish_sorted_str(children, depth, indenter)
+    result = _stylish_sorted_str(children, depth, indenter)
     return result
 
 
-def parse_value(value_view, depth=1):
-    """ Value formatter that interprets diff
-    for proper result generation. """
+def _parse_value(value_view, depth=1):
+    """Generic value parser and formatter"""
     if is_dict(value_view):
-        # generate value using parser for equal values
+        # parse nested value
         next_lvl = depth + 1
-        value = stylish_formatted_equals(value_view, next_lvl)
+        value = _stylish_formatted_equals(value_view, next_lvl)
     else:
-        # generate value depending on type
-        value = get_value_from(value_view)
+        # parse flat value
+        value = _get_value_from(value_view)
     return value
 
 
-def format_stylish(diff: dict, depth=1) -> str:
-    """ Convert diff to json-like string. """
-    blank = list()
-    indenter = ' ' * 4 * (depth - 1)
-    # iterate diff complex keys
-    for sign, key in diff:
-        value_view = diff[(sign, key)]
-        if sign == ' ':
-            next_lvl = depth + 1
-            # sign ' ' means we need to run recursion
-            # to get value
-            value = format_stylish(value_view, next_lvl)
-            line = f'    {key}: {value}'
-        else:
-            value = parse_value(value_view, depth)
-        # generate whole line
-        if sign == '=':
-            # key didn't change
-            line = f'    {key}: {value}'
-            blank.append(indenter + line)
-        elif sign == '-':
-            line = f'  {sign} {key}: {value}'
-            try:
-                # key updated
-                comparison_value = diff[('+', key)]
-                comparison_value = parse_value(comparison_value, depth)
-                line2 = f'  + {key}: {comparison_value}'
-                blank.append(indenter + line + '\n' + indenter + line2)
-            except KeyError:
-                # key removed
-                blank.append(indenter + line)
-        else:
-            try:
-                comparison_value = diff[('-', key)]
-                continue
-            except KeyError:
-                # key added
-                line = f'  {sign} {key}: {value}'
-                blank.append(indenter + line)
-
-    return stylish_sorted_str(blank, depth, indenter)
-
-
-def get_name(key, ancestry):
+def _get_node_name(key, ancestry):
     node_ancestry = copy.copy(ancestry)
     node_ancestry.append(key)
-    name = '.'.join(node_ancestry)
+    name = ".".join(node_ancestry)
     return name
 
 
-def value_formatter(value_view):
+def _plain_value_formatter(value_view):
     if is_dict(value_view):
-        value = '[complex value]'
+        value = "[complex value]"
     elif isinstance(value_view, str):
-        value = '\'' + value_view + '\''
+        value = "'" + value_view + "'"
     else:
         # translate plain updated value back to json
-        value = get_value_from(value_view)
+        value = _get_value_from(value_view)
     return value
 
 
 def walk_plain(node, ancestry: list, blank: list):
     for sign, key in node:
-        if sign == '-':
+        if sign == "-":
             # check if key was updated or just removed
             try:
                 # if key was updated there is new value for it
-                updated_value_view = node[('+', key)]
+                updated_value_view = node[("+", key)]
             except KeyError:  # if no new value, key was removed
                 # accumulate name
-                name_removed = get_name(key, ancestry)
+                name_removed = _get_node_name(key, ancestry)
                 # Gen output
-                blank.append(f'Property \'{name_removed}\' was removed')
+                blank.append(f"Property '{name_removed}' was removed")
             else:
                 # accumulate name and new value
-                name_updated = get_name(key, ancestry)
-                updated_value = value_formatter(updated_value_view)
+                name_updated = _get_node_name(key, ancestry)
+                updated_value = _plain_value_formatter(updated_value_view)
 
                 # get and translate initial value
-                initial_value_view = node[('-', key)]
-                initial_value = value_formatter(initial_value_view)
+                initial_value_view = node[("-", key)]
+                initial_value = _plain_value_formatter(initial_value_view)
                 # gen output line
-                blank.append(f'Property \'{name_updated}\' was updated. '
-                             f'From {initial_value} to {updated_value}')
+                blank.append(
+                    f"Property '{name_updated}' was updated. "
+                    f"From {initial_value} to {updated_value}"
+                )
 
-        elif sign == '+':
+        elif sign == "+":
             try:
-                _ = node[('-', key)]
+                _ = node[("-", key)]
                 continue  # avoid duplicates, diff is added at previous step
             except KeyError:  # means key was added
                 # accumulate name
-                name_added = get_name(key, ancestry)
-                added_value_view = node[('+', key)]
-                added_value = value_formatter(added_value_view)
-                blank.append(f'Property \'{name_added}\' was added '
-                             f'with value: {added_value}')
+                name_added = _get_node_name(key, ancestry)
+                added_value_view = node[("+", key)]
+                added_value = _plain_value_formatter(added_value_view)
+                blank.append(
+                    f"Property '{name_added}' was added " f"with value: {added_value}"
+                )
 
-        elif sign == ' ':
-            next_node = node[(' ', key)]
+        elif sign == " ":
+            next_node = node[(" ", key)]
             new_ancestry = copy.copy(ancestry)
             new_ancestry.append(key)
             walk_plain(next_node, new_ancestry, blank)
 
 
-def format_plain(diff: dict) -> str:
-    '''Human-friendly output formatter'''
-    blank: List[str] = list()
-    ancestry: List[str] = list()
-    walk_plain(diff, ancestry, blank)
-    result = '\n'.join(sorted(blank, key=lambda x: x[10:]))
-    return result
-
-
-def format_json(diff: dict) -> str:
-    """Return diff as json"""
-    blank = dict()
-    for sign, key in diff:
-        keyword = f'{sign} {key}'
-        equal_keyword = f'  {key}'
-        value = diff[(sign, key)]
-        if sign == '=':
-            blank[equal_keyword] = value
-        elif sign == ' ':
-            blank[keyword] = format_stylish(value)
-        else:
-            blank[keyword] = value
-    result = json.dumps(blank)
-    return result
+def is_dict(obj: Any) -> bool:
+    return isinstance(obj, dict)
